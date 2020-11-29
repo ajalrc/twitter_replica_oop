ec3a9ac6660937e9d06e42f23fa0ce715d2ef2cf
ajalrc <61206336+ajalrc@users.noreply.github.com>
2020-11-28 18:24:28 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,28 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        Twitter twitter= new Twitter();
+        twitter.postTweet(1,5,"The best teacher for the success is Mr.failure");
 
+        // User 1's news feed should return a list with 1 tweet id -> [5].
+        System.out.println(twitter.getNewsFeed(1));
+
+// User 1 follows user 2.
+        twitter.follow(1, 2);
+
+// User 2 posts a new tweet (id = 6).
+        twitter.postTweet(2, 6,"Definitely true and the second teacher is the empty pocket");
+
+// User 1's news feed should return a list with 2 tweet ids -> [6, 5].
+// Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+        System.out.println(twitter.getNewsFeed(1));
+
+// User 1 unfollows user 2.
+        twitter.unfollow(1, 2);
+
+// User 1's news feed should return a list with 1 tweet id -> [5],
+// since user 1 is no longer following user 2.
+        System.out.println(twitter.getNewsFeed(1));
     }
 }
 class Twitter {
@@ -12,10 +33,12 @@ class Twitter {
         int tweettime;
         int tweetid;
         tweet next;
-        public tweet(int tweetid,int tweettime){
+        String message;
+        public tweet(int tweetid,int tweettime,String message){
             this.tweetid=tweetid;
             this.tweettime=tweettime;
             this.next=null;
+            this.message=message;
         }
     }
 
@@ -34,7 +57,7 @@ class Twitter {
     /**
      * Compose a new tweet.
      */
-    public void postTweet(int userId, int tweetId) {
+    public void postTweet(int userId, int tweetId,String message) {
         /**
          * Here first I am looking if that user is in the followers table and so if he is
          * then he might have been tweeting from before so, I need to make sure that his latest
@@ -42,12 +65,12 @@ class Twitter {
          * else we just add the tweet in the hashmap tweets
          */
         if(collecttweet.containsKey(userId)){
-            tweet tweet=new tweet(tweetId,time++);
+            tweet tweet=new tweet(tweetId,time++,message);
             tweet.next=collecttweet.get(userId);
             collecttweet.put(userId,tweet);
         }
         else{
-            collecttweet.put(userId,new tweet(tweetId,time++));
+            collecttweet.put(userId,new tweet(tweetId,time++,message));
         }
 
     }
@@ -56,7 +79,7 @@ class Twitter {
      * Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
      */
 
-    public List<Integer> getNewsFeed(int userId) {
+    public List<Dictionary<Integer,String>> getNewsFeed(int userId) {
         //System.out.println(collecttweet);
         //System.out.println(collectusers);
         /**
@@ -65,7 +88,7 @@ class Twitter {
          * people have any tweets and all all the tweets in the queue and then comapre the time when
          * they were posted and have that order in the queue.
          */
-        List<Integer> list=new ArrayList<>();
+        List<Dictionary<Integer,String>> list=new ArrayList<>();
         PriorityQueue<tweet> priorityQueue=new PriorityQueue<>(new Comparator<tweet>() {
             /**
              * Baiscally the comparator uses this logic to compare the time for which the tweets were posted.
@@ -92,7 +115,9 @@ class Twitter {
 
         while(!priorityQueue.isEmpty() && list.size()<10){
             tweet nexttweet=priorityQueue.poll();
-            list.add(nexttweet.tweetid);
+            Dictionary<Integer,String> dictionary = new Hashtable<>();
+            dictionary.put(nexttweet.tweettime,nexttweet.message);
+            list.add(dictionary);
             //this below line is to make sure that if the same user has the tweet then arrange that in priority queue based
             //on the time it was posted.
             if(nexttweet.next!=null) priorityQueue.offer(nexttweet.next);
